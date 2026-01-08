fc106b3bcf9e59d31a06a63300d80d7eda2d8dd8
italianoW <wendelitaliano2@gmail.com>
2026-01-07 20:41:45 -0500

    java_ls

--- lua/plugins/lsp/mason-lsp.lua
+++ lua/plugins/lsp/mason-lsp.lua
@@ -1,7 +1,7 @@
 return{
     "williamboman/mason-lspconfig.nvim",
   opts = {
-      ensure_installed = { "lua_ls", "ts_ls" }, 
+      ensure_installed = { "lua_ls", "ts_ls","java_language_server" }, 
     }
   }
 
